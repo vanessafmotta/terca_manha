c0f540953a37cb203759f2ba9c7503db05abe96a
Vanessa Motta <89320692+vanessafmotta@users.noreply.github.com>
2022-03-29 10:09:33 -0300

    continuação-arrays-loops

--- php-uni9/2_arrays_loops/array.php
+++ php-uni9/2_arrays_loops/array.php
@@ -10,27 +10,45 @@
 
 <body>
 	<?php
-	// array sequencial
-	$lista_compras = array('Ovos', 'Bananas', 'Leite', 'Chocolate');
-	$list = ['Frutas', 'Pão', 'Arroz', 3, 1.5];
+	//array sequencial
+	$lista_compras = array("Ovos", "Bananas", "Leite", "Chocolate", "Farinha");
+	$lista = ["Frutas", 3, "Arroz", 1123131231231.532323123, true];
 
-	echo '<pre>';
+	echo "<pre>";
 	var_dump($lista_compras);
-	echo '</pre>';
+	echo "</pre>";
 
 	echo "<hr>";
 
-	echo '<pre>';
-	var_dump($list);
-	echo '</pre>';
+	echo "<pre>";
+	var_dump($lista);
+	echo "</pre>";
 
 	echo "<hr>";
 
-	echo '<pre>';
-	var_dump($list[2]);
-	echo '</pre>';
+	echo "<pre>";
+	var_dump($lista[2]);
+	echo "</pre>";
 
-	// paramos aqui ... falta "array associativo"
+	echo "<hr>";
+
+	//"array associativo"
+	$lista['x'] = 'Uninove';
+
+	echo "<pre>";
+	var_dump($lista);
+	echo "</pre>";
+
+	echo "<hr>";
+
+	$dados_aluno["nome"] = "Thiago G. Traue";
+	$dados_aluno["nota"] = 9;
+	$dados_aluno["email"] = "thiago.traue@uni9.pro.br";
+	$dados_aluno["aprovado"] = true;
+
+	echo "<pre>";
+	var_dump($dados_aluno);
+	echo "</pre>";
 	?>
 </body>
 
